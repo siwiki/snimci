497e140033024fdafcdb956b245dcbf157da4fe4
Luka Simić <git@kocka.tech>
2023-09-06 13:55:21 +0200

    Prevent navigation from index if combining keys. Otherwise, users could use Ctrl+num keys to navigate and be accidentally redirects to semester pages.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,9 @@
     var semesters = null;
 
     function onKeyDown(event) {
+        if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
+            return;
+        }
         var number = Number(event.key);
         if (number > 0 && number < 9) {
             semesters.children[number - 1].click();
